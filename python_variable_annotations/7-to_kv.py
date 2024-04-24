7e7a82b237797a4376f064a3ad72a0756606964c
Mamuche <marionlaroche17@gmail.com>
2024-04-24 15:42:21 +0200

    task7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,10 +3,10 @@
 an int OR float v as arguments and returns a tuple. The first
 element of the tuple is the string k. The second element is the
 square of the int/float v and should be annotated as a float."""
-from typing import Union
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple containing the string k and
     the square of the int/float v"""
     return (k, v * v)
