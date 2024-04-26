e5bcd99a7fab3934a95828c948f656740dcd0929
Mamuche <marionlaroche17@gmail.com>
2024-04-26 15:33:40 +0200

    task2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
-"""Import async_generator from the previous task and
-then write a coroutine called async_comprehension that
-takes no arguments.
+"""Import async_comprehension from the previous
+file and write a measure_runtime coroutine that
+will execute async_comprehension four times in parallel
+using asyncio.gather.
 
-The coroutine will collect 10 random numbers using an
-async comprehensing over async_generator, then return the 10 random numbers."""
+measure_runtime should measure the total runtime and
+return it.
+
+Notice that the total runtime is roughly 10 seconds,
+explain it to yourself."""
 import asyncio
-from typing import List
+import time
 
-async_generator = __import__('0-async_generator').async_generator
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def async_comprehension() -> List[float]:
-    """Return a list of 10 random numbers using async comprehension."""
-    return [i async for i in async_generator()]
+async def measure_runtime() -> float:
+    """Measures the total runtime and returns it"""
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    end_time = time.time()
+
+    return end_time - start_time
