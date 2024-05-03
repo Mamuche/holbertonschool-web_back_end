30efd90223b7248bd80f3d6bf3dcc3b67615f516
Mamuche <marionlaroche17@gmail.com>
2024-05-03 09:40:45 +0200

    task1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""""""
+"""Implement a method named get_page that takes two integer arguments page with
+default value 1 and page_size with default value 10."""
 from typing import Tuple, List
 import csv
 import math
@@ -11,16 +12,14 @@ def index_range(page: int, page_size: int) -> tuple:
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -30,6 +29,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """return the appropriate page of the dataset"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         start_idx, end_idx = index_range(page, page_size)
