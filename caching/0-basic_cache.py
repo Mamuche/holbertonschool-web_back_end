7cc5dcd0cdbd5c4b0232f84c17471d4798b5dcc1
Mamuche <marionlaroche17@gmail.com>
2024-12-16 16:05:48 +0100

    pycodstyle

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -7,15 +7,16 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """caching system without limit"""
+    """Caching system without limit"""
     def put(self, key, item):
-        """method inerited from BaseCaching. Puts key and item in cache_data"""
+        """Method inerited from BaseCaching. Puts key and item in cache_data"""
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """method inerited from BaseCaching. Returns the value linked to key"""
+        """Method inerited from BaseCaching. Returns the value linked to key"""
         if key in self.cache_data:
             return self.cache_data[key]
         return None
+
     
--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -17,7 +17,7 @@ class FIFOCache(BaseCaching):
         if key and item:
             self.order.append(key)
             self.cache_data[key] = item
-        
+
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_key = self.order.pop(0)
             del self.cache_data[first_key]
@@ -28,4 +28,5 @@ class FIFOCache(BaseCaching):
         if key in self.cache_data:
             return self.cache_data[key]
         return None
+
     
