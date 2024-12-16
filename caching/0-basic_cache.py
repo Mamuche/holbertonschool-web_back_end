6d027a230f4c9471cdb30151ea420c1c50003222
Mamuche <marionlaroche17@gmail.com>
2024-12-16 12:14:35 +0100

    Requirements task0

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -7,14 +7,14 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """"""
+    """caching system without limit"""
     def put(self, key, item):
-        """"""
+        """method inerited from BaseCaching. Puts key and item in cache_data"""
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """"""
+        """method inerited from BaseCaching. Returns the value linked to key"""
         if key in self.cache_data:
             return self.cache_data[key]
         return None
