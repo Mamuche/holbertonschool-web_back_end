c970299acb0bd67a516702bc7577162d4acf7396
Mamuche <marionlaroche17@gmail.com>
2024-12-17 12:59:00 +0100

    task3

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,1 +1,33 @@
 #!/usr/bin/env python3
+""""""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRU caching system"""
+    def __init__(self):
+        """ Initialize class """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """Method inerited from BaseCaching. Puts key and item in cache_data"""
+        if key and item:
+            if key in self.order:
+                self.order.remove(key)
+            self.order.append(key)
+            self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            lru_key = self.order.pop(0)
+            del self.cache_data[lru_key]
+            print(f"DISCARD: {lru_key}")
+
+    def get(self, key):
+        """Method inerited from BaseCaching. Returns the value linked to key"""
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
