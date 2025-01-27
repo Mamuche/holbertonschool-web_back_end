d8d2e3de2a71a1ec06bdf5ca33312a8a114a3ddb
Mamuche <marionlaroche17@gmail.com>
2025-01-27 15:15:38 +0100

    fix requirements

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Write a function called filter_datum that returns the log message obfuscated
 """
 
 import re
+from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
