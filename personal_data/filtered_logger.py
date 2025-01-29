f2f9934a719c5ac65aad1a724f08c6b1983507c0
Mamuche <marionlaroche17@gmail.com>
2025-01-29 12:07:23 +0100

    task 2

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,6 +6,11 @@ Write a function called filter_datum that returns the log message obfuscated
 import re
 from typing import List
 import logging
+import mysql.connector
+import os
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 class RedactingFormatter(logging.Formatter):
@@ -41,3 +46,55 @@ def filter_datum(
         msg = f"{field}=[^{separator}]+"
         message = re.sub(msg, f"{field}={redaction}", message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """returns a loggin.Logger object"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+    return logging
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to the database """
+    connection = mysql.connector.connection.MySQLConnection(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    )
+    return connection
+
+
+def main():
+    """ Main function """
+    db = get_db()
+    cursor = db.cursor()
+    query = """
+    SELECT name, email, phone, ssn, password, ip, last_login, user_agent
+    FROM users;
+    """
+    cursor.execute(query)
+    logger = get_logger()
+    for row in cursor.fetchall():
+        record = {
+            "name": row[0],
+            "email": row[1],
+            "phone": row[2],
+            "ssn": row[3],
+            "password": row[4],
+            "ip": row[5],
+            "last_login": row[6],
+            "user_agent": row[7],
+        }
+        message = "; ".join(f"{key}={value}" for key,
+                            value in record.items()) + ";"
+        message = filter_datum(PII_FIELDS, '***', message, '; ')
+        logger.info(message)
+    cursor.close()
+    db.close()
+
