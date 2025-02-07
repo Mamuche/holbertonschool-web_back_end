4e1f87c508b5600dc1419b6ca3de551310107312
Mamuche <marionlaroche17@gmail.com>
2025-02-07 13:19:43 +0100

    task2

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -25,6 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized():
     """ GET /api/vi/unauthorized
@@ -32,3 +33,12 @@ def unauthorized():
       - 401 error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """ GET /api/v1/forbidden
+    Return:
+      - 403 error
+    """
+    abort(403)
