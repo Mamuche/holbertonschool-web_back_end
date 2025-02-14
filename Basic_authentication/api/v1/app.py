eb30476f093c61c0926731fb2ea2605ff8b2b59a
Mamuche <marionlaroche17@gmail.com>
2025-02-14 10:04:49 +0100

    task 5

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -6,12 +6,22 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.auth import Auth
 import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
+auth = None
+
+
+auth_type = os.getenv("AUTH_TYPE", None)
+
+if auth_type == "auth":
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -33,6 +43,35 @@ def forbidden_error(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """
+    if auth is None, do nothing
+    if request.path is not part of this list
+    ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'],
+    do nothing - you must use the method require_auth from the auth instance
+    if auth.authorization_header(request) returns None,
+    raise the error 401 - you must use abort
+    if auth.current_user(request) returns None, raise the error 403 -
+    you must use abort
+    """
+    if auth is None:
+        return
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,14 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        returns False - path and excluded_paths will be used later
+        returns True if the path is not in the list of strings excluded_paths:
+        Returns True if path is None
+        Returns True if excluded_paths is None or empty
+        Returns False if path is in excluded_paths
+        You can assume excluded_paths contains string path always ending by a /
+        This method must be slash tolerant:
+        path=/api/v1/status and path=/api/v1/status/
+        must be returned False if excluded_paths contains /api/v1/status/
         """
         if path is None:
             return True
@@ -27,9 +34,17 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """
-        returns None - request will be the Flask request object
+        If request is None, returns None
+        If request doesn't contain the header key Authorization, returns None
+        Otherwise, return the value of the header request Authorization
         """
-        return None
+        if request is None:
+            return None
+
+        if "Authorization" not in request.headers:
+            return None
+
+        return request.headers.get("Authorization")
 
     User = TypeVar("User")
 
