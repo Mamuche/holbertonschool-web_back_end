03be58bdbce2b2f653e57f88fac47e3ee27874da
Mamuche <marionlaroche17@gmail.com>
2025-02-28 09:38:10 +0100

    task 0

---
+++ Session_authentication/README.md
@@ -0,0 +1,28 @@
+<p align="center">
+<picture>
+ <source media="(prefers-color-scheme: dark)" srcset="https://images.squarespace-cdn.com/content/v1/5a4bfe8bf09ca4228ceca3b7/1539139199598-ANH454IHZI1OKWONKRXY/logo.jpg?format=2500w">
+ <source media="(prefers-color-scheme: light)" srcset="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIrK23KvJPB7XdZrIk9mHwe3GZvtsUZLjkh-eG6KRgCLeWu3MW0kFcggq4COpLmeZviQ&usqp=CAU">
+ <img alt="image holberton school" src="https://apply.holbertonschool.com/auth/sign_up?country=fr&locale=fr">
+</picture>
+</p>
+
+
+<B><h1 align="center">
+Session authentication
+</h1></B>
+
+## **Topics Covered**
+
+0. Et moi et moi et moi!
+1. Empty session
+2. Create a session
+3. User ID for Session ID
+4. Session cookie
+5. Before request
+6. Use Session ID for identifying a User
+7. New view for Session Authentication
+8. Logout
+
+## **Authors** :black_nib:
+
+Laroche Marion [Github](https://github.com/Mamuche), [LinkedIn](https://www.linkedin.com/in/marion-laroche-8b235a284/).
---
+++ Session_authentication/api/v1/app.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
+auth = None
+auth_type = os.getenv("AUTH_TYPE", None)
+
+if auth_type == "basic_auth":
+    auth = BasicAuth()
+elif auth_type == "auth":
+    auth = Auth()
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized_error(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """
+    if auth is None, do nothing
+    if request.path is not part of this list
+    ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'],
+    do nothing - you must use the method require_auth from the auth instance
+    if auth.authorization_header(request) returns None,
+    raise the error 401 - you must use abort
+    if auth.current_user(request) returns None, raise the error 403 -
+    you must use abort
+    """
+    print(f"Debug: auth = {auth}")
+
+    if auth is None:
+        return
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        print("Debug: No Authorization header found")
+        abort(401)
+
+    request.current_user = auth.current_user(request)
+    print(f"Debug: request.current_user = {request.current_user}")
+
+    if auth.current_user(request) is None:
+        print("Debug: current_user() returned None")
+        abort(403)
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
