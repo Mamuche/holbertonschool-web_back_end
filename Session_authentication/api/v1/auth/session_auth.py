0b311f1a26285205d17cd76720162e237199804e
Mamuche <marionlaroche17@gmail.com>
2025-02-28 10:45:51 +0100

    task3

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -9,7 +9,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ Crée un Session ID pour un user_id donné """
+        """ that creates a Session ID for a user_id """
         if user_id is None or not isinstance(user_id, str):
             return None
 
@@ -18,3 +18,10 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns a User ID based on a Session ID """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
