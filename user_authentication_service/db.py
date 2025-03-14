7bf7b9febf80dfec53becb2ab07d5cc0993a022b
Mamuche <marionlaroche17@gmail.com>
2025-03-14 15:44:01 +0100

    task2

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -37,3 +39,20 @@ class DB:
         session.add(new_user)
         session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        takes in arbitrary keyword arguments and returns
+        the first row found in the users table
+        """
+        session = self._session
+        try:
+            return session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound(
+                "No user found matching the provided criteria."
+                )
+        except AttributeError:
+            raise InvalidRequestError(
+                "Invalid request: Check the arguments passed to the query."
+                )
