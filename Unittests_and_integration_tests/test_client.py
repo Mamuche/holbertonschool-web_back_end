2df0df80eb99faf69732904f8b5aa27dc3c40ee8
Mamuche <marionlaroche17@gmail.com>
2025-03-27 14:26:33 +0100

    task5

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """A github org client"""
 import unittest
-from unittest.mock import patch
+from unittest import TestCase
 from parameterized import parameterized
+from unittest.mock import patch, PropertyMock
 
 from client import GithubOrgClient
 
@@ -17,7 +18,7 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch("client.get_json")
     # remplace get_json par mock_get_json
     def test_org(self, org_name, mock_get_json):
-        """Test that GithubOrgClient.org returns correct data"""
+        """Test for GithubOrgClient"""
         expected_payload = {"name": org_name}
         mock_get_json.return_value = expected_payload
 
@@ -28,3 +29,18 @@ class TestGithubOrgClient(unittest.TestCase):
             f"https://api.github.com/orgs/{org_name}"
         )
         self.assertEqual(result, expected_payload)
+
+    def test_public_repos_url(self):
+        """Test for _public_repos_url"""
+        test_payload = {
+            "repos_url": "https://api.github.com/orgs/google/repos"
+        }
+        # patch pour une propriété de classe (propertyMock)
+        with patch.object(
+            GithubOrgClient, "org", new_callable=PropertyMock
+        ) as mock_org:
+            mock_org.return_value = test_payload
+            client = GithubOrgClient("google")
+            result = client._public_repos_url
+
+            self.assertEqual(result, test_payload["repos_url"])
