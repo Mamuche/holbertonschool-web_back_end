eecbe4544de28ed226fa4d30c55974ba36464974
Mamuche <marionlaroche17@gmail.com>
2025-04-04 11:45:29 +0200

    add comment

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ """
+        """ Takes a data argument and returns a string """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
