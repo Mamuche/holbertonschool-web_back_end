c2c6d84379c6cbfb740b980bd427f85dceabe00d
Mamuche <marionlaroche17@gmail.com>
2025-05-19 14:09:46 +0200

    i give up

---
+++ queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,33 @@
+import pkg from 'redis';
+const { createClient } = pkg;
+const { print } = pkg.default || pkg;
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.get(schoolName, (err, result) => {
+    console.log(result);
+  });
+};
+
+const main = async () => {
+  await client.connect();
+
+  displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  displaySchoolValue('HolbertonSanFrancisco');
+};
+
+main();
